7ffd156acda8aea4d98a2c95bb1961fd8e6938e8
Timothy Kum'bweza Banda <timobify@gmail.com>
2024-07-11 16:28:52 +0200

    Fix: used environmental variables on key

--- main/settings.py
+++ main/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$_ucca)d$2b**s$gd%h_y*aq14v2)gdrszacj=n*-g04660%=7'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
